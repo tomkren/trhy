7fe0c45de285e6e6f39d37bbdcfe6baa0c43b620
Tom Kren <tomkren@gmail.com>
2014-05-10 00:47:20 +0200

    uprostřed zjednodušování okolo komodit mašin atd

--- src/cz/tomkren/trhy/Commodity.java
+++ src/cz/tomkren/trhy/Commodity.java
@@ -18,13 +18,28 @@ public abstract class Commodity {
         public String getName() {return name;}
     }
 
-    public static class Mach extends Commodity {
+    // todo zbavit se tohodle (hlavni demence: kaplý s mašinou, netřeba), Arrow je lepší (cheme Comoditu jako ekviv typu)
+    /*public static class Mach extends Commodity {
         private Machine machine;
         public Mach(Machine m) {
             machine = m;
         }
         public String getName() {
             return machine.getName();
+        }
+    }*/
+
+    public static class Arrow extends Commodity {
+        private Commodity input;
+        private Commodity output;
+
+        public Arrow(Commodity input, Commodity output) {
+            this.input = input;
+            this.output = output;
+        }
+
+        public String getName() {
+            return "( "+ input.getName() +" -> "+ output.getName() +" )";
         }
     }
     
