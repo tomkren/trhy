21e3f1d07982c5c2bceb50013a98c6baae4b8f88
Tom Kren <tomkren@gmail.com>
2014-05-11 12:26:45 +0200

    Comb vylepšení

--- src/cz/tomkren/fishtron/Comb.java
+++ src/cz/tomkren/fishtron/Comb.java
@@ -4,17 +4,39 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public interface Combinator {
-    public Combinator apply(Combinator comb);
+public interface Comb {
+    public Comb ap(Comb comb);
 
-    public static final Combinator plus = new Fun2<Integer,Integer,Integer>("plus", (x,y)-> x+y);
-    public static final Combinator succ = new Fun<Integer,Integer>("succ", x -> x+1);
-    public static final Combinator _0   = new Val<>(0);
-    public static final Combinator _1   = new Val<>(1);
-    public static final Combinator _2   = new Val<>(2);
+    default public Comb ap (int i) {
+        return ap(new Val<Integer>(i));
+    }
 
+    public static final Comb plus = new Fun2<Integer,Integer,Integer>("plus", (x,y)-> x+y);
+    public static final Comb succ = new Fun<Integer,Integer>("succ", x -> x+1);
+    public static final Comb _0   = new Val<>(0);
+    public static final Comb _1   = new Val<>(1);
+    public static final Comb _2   = new Val<>(2);
+    public static final Comb K_   = x -> (y -> x);
+    public static final Comb K    = mk("K", x -> (y -> x) );
+    public static final Comb S    = mk("S", f -> (g -> (x -> f.ap(x).ap(g.ap(x)) )));
 
-    public class Fun<A,B> implements Combinator {
+    public static Comb mk(String str, Comb f){
+        return new Comb() {
+            public Comb ap(Comb x) {
+                Comb result = f.ap(x);
+                if (result instanceof Val) {
+                    return result;
+                } else {
+                    return mk("("+str+" "+x.toString()+")", result);
+                }
+            }
+            public String toString() {
+                return str;
+            }
+        };
+    }
+
+    public class Fun<A,B> implements Comb {
         private Function<A,B> f;
         private String sym;
         private Supplier<String> toStr;
@@ -29,7 +51,7 @@ public interface Combinator {
             this.f = f;
             this.toStr = toStr;
         }
-        public Combinator apply(Combinator x) {
+        public Comb ap(Comb x) {
             return new Val<B>(f.apply(((Val<A>) x).get()));
         }
         @Override public String toString() {
@@ -43,7 +65,7 @@ public interface Combinator {
         }
     }
 
-    public class Fun2<A,B,C> implements Combinator {
+    public class Fun2<A,B,C> implements Comb {
         private BiFunction<A,B,C> f;
         private String sym;
         private Supplier<String> toStr;
@@ -58,7 +80,7 @@ public interface Combinator {
             this.f = f;
             this.toStr = toStr;
         }
-        public Combinator apply(Combinator x) {
+        public Comb ap(Comb x) {
             return new Fun<B,C>( y -> f.apply(((Val<A>) x).get(), y) ,
                                 () -> "( "+ sym +" "+ ((Val<A>) x).toString() + " )" );
         }
@@ -73,7 +95,7 @@ public interface Combinator {
         }
     }
 
-    public class Val<T> implements Combinator {
+    public class Val<T> implements Comb {
         private T x;
         public Val(T x) {
             this.x = x;
@@ -81,7 +103,7 @@ public interface Combinator {
         public T get() {
             return x;
         }
-        public Combinator apply(Combinator comb) {
+        public Comb ap(Comb comb) {
             throw new UnsupportedOperationException("Val<T> cannot by applied.");
         }
         @Override public String toString() {
@@ -92,14 +114,26 @@ public interface Combinator {
     public static void main(String[] args) {
         Log.it("Hello Combinatorz!");
         Log.it( _0 );
-        Log.it( succ.apply(_0) );
-        Log.it( plus.apply(_2).apply(_2) );
-        Log.it( plus.apply(_1) );
+        Log.it( succ.ap(0) );
+        Log.it( plus.ap(2).ap(2) );
+        Log.it( plus.ap(1) );
         Log.it( succ );
         Log.it( plus );
-        Log.it( succ.apply( plus.apply(_2).apply(_2) ) );
+        Log.it( succ.ap(plus.ap(2).ap(2)) );
 
+        Log.it().it( "K_:"              );
+        Log.it( K_.ap(2).ap(1)            );
+        Log.it( K_.ap(2)                 );
+        Log.it( K_.ap(K_).ap(1).ap(2).ap(3) );
 
+        Log.it().it( "K:"              );
+        Log.it( K.ap(2).ap(1)            );
+        Log.it( K.ap(2)                 );
+        Log.it( K.ap(K).ap(1).ap(2).ap(3)  );
+
+        Log.it().it("S:");
+        Log.it( "S K K    = "+ S.ap(K).ap(K)       );
+        Log.it( "S K K 42 = "+ S.ap(K).ap(K).ap(42) );
 
     }
 }
